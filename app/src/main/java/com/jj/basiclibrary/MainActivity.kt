bcf11953a8832b32a14d9a09cf23136435beddbb
jamie <bluegigsolutions@gmail.com>
2022-08-23 11:29:55 +0100

    completed library

--- app/src/main/java/com/jj/basiclibrary/MainActivity.kt
+++ app/src/main/java/com/jj/basiclibrary/MainActivity.kt
@@ -2,10 +2,21 @@ package com.jj.basiclibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.jj.blinklibrary.BlinkEffect
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.buttonView)
+        btn.setOnClickListener {
+            // use of blink-library
+            BlinkEffect.blink(btn)
+        }
     }
 }
